65af532c28076c4d2ce1d4c0755f4fd2e34cec61
NazarZbrizkiy <nazarzbrizkiy@knu.ua>
2025-05-02 18:35:42 +0300

    Commit 5

--- Ntnhsc.py
+++ Ntnhsc.py
@@ -1,5 +1,3 @@
-from itertools import count
-
 x = (1,2,3,4,5,6,7,8,9)
 print(x)
 b=1
@@ -8,4 +6,5 @@ def plus(a,b):
     c = a + b
     return c
 print(plus(a,b))
-
+lst = [1,2,3,4,5,6,7,8,9,10]
+print(lst)
